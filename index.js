223f8de237d693a53c1be823a60bd0338eb0fbed
Shanice Chebet Kenduiwa <kenduiwashanice63@gmail.com>
2024-04-03 18:14:06 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the main#main node
+const mainNode = document.querySelector('main#main');
+mainNode.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// Set the inner text of the new <h1> element to 'YOUR-NAME is the champion'
+newHeader.textContent = 'Shanice is the champion'; // Replaced YOUR-NAME with  name
+
+// Append the new <h1> element to the document body or any other appropriate location
+document.body.appendChild(newHeader);
